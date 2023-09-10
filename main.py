34bad381f69b060b9406be4fffea385ed0179754
Mansour Alshebli <mansour.m.alsheblli@gmail.com>
2023-09-10 22:38:32 +0400

    runing flask

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 # @mansourshebli
+
+from flask import Flask
+
+app = Flask(__name__)
+
+# using python decorator to add functionality to the website
+@app.route("/apple-news")
+def index():
+    return "Apple News!"
+
+app.run(host="0.0.0.0", port=80)
