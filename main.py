98dd586d42bbd2d53b9e1f0c0b7eb3afd7b80666
Mansour Alshebli <mansour.m.alsheblli@gmail.com>
2023-09-11 16:36:00 +0400

    fixing errors @main.py

--- main.py
+++ main.py
@@ -1,12 +1,50 @@
 # @mansourshebli
 
-from flask import Flask
+from flask import Flask, render_template, jsonify
 
+# https://docs.python.org/3/library/json.html
+# This library will be used to parse the JSON data returned by the API.
+import json
+
+# https://docs.python.org/3/library/urllib.request.html#module-urllib.request
+# This library will be used to fetch the API.
+import urllib.request
+
+# App configuration
 app = Flask(__name__)
+app.run(host="0.0.0.0", port=80)
 
-# using python decorator to add functionality to the website
+# News function (uses api key from GNews)
+def get_news():
+    apikey = "910f4ef48b858659cf53b9f030a88c88"
+    category = "general"
+    url = f"https://gnews.io/api/v4/top-headlines?category={category}&lang=en&country=us&max=10&apikey={apikey}"
+
+    with urllib.request.urlopen(url) as response:
+        data = json.loads(response.read().decode("utf-8"))
+        articles = data["articles"]
+
+        # Create a list to store the news articles
+        news_list = []
+
+        for article in articles:
+            news_item = {
+                "title": article["title"],
+                "description": article["description"],
+                "url": article["url"],
+                "image": article["image"],
+                "publishedAt": article["publishedAt"],
+                "source": article["source"]["name"]
+            }
+            news_list.append(news_item)
+
+        return news_list
+
+# Route to display news
 @app.route("/")
 def index():
-    return "Apple News!"
+    news_data = get_news()
+    return render_template("index.html", news=news_data)
 
-app.run(host="0.0.0.0", port=80)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=500, debug=True)
